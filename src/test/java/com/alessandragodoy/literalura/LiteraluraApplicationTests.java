733fced2cb495240c90b3ad9cfa74d82f5dd8f7a
abengl <126703861+abengl@users.noreply.github.com>
2024-12-29 12:26:18 -0500

    :tada: init: base project

---
+++ src/test/java/com/alessandragodoy/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessandragodoy.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
